228fcf73288d9a565ac613958c465ed4445c1dd0
Tomasz Watras <watrix@MacBookPro.lan>
2024-12-15 19:09:45 +0100

    poprawki w stripe

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-      },
+    domains: [
+      'firebasestorage.googleapis.com',
     ],
-  }
+    unoptimized: true, 
+  },
 }
 
 module.exports = nextConfig
