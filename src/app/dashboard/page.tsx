10db8e7d2ebfd3a1d1a95a6fb2ed271016bd6426
Tomasz Watras <watrix@MacBookPro.lan>
2024-12-15 20:50:21 +0100

    landing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,32 @@
-// 'use client';
+'use client';
 
-// import { useEffect, useState } from 'react';
-// import { useRouter } from 'next/navigation';
-// import { useAuth } from '@/lib/contexts/auth';
-// import LandingPage from '@/app/components/LandingPage';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/lib/contexts/auth';
+import LandingPage from '../page';
 
-// export default function HomePage() {
-//   const router = useRouter();
-//   const { user, loading } = useAuth();
-//   const [isLoading, setIsLoading] = useState(true);
+export default function HomePage() {
+  const router = useRouter();
+  const { user, loading } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
-//   useEffect(() => {
-//     if (!loading) {
-//       if (user) {
-//         router.push('/dashboard/waiter');
-//       } else {
-//         setIsLoading(false);
-//       }
-//     }
-//   }, [user, loading, router]);
+  useEffect(() => {
+    if (!loading) {
+      if (user) {
+        router.push('/dashboard/waiter');
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [user, loading, router]);
 
-//   if (isLoading || loading) {
-//     return (
-//       <div className="min-h-screen flex items-center justify-center">
-//         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500" />
-//       </div>
-//     );
-//   }
+  if (isLoading || loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500" />
+      </div>
+    );
+  }
 
-//   return <LandingPage />;
-// }
+  return <LandingPage />;
+}
