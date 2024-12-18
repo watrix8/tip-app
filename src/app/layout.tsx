038e9abbf0db1db819ba57de42958ec4263f63ea
Tomasz Watras <watrix@MacBook-Pro-Tomasz-2.local>
2024-12-18 12:57:19 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,32 @@
 import './globals.css';
 import { AuthProvider } from '@/lib/contexts/auth';
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'tippin\' - Przyjmuj napiwki elektronicznie',
+  description: 'Przyjmuj napiwki kartą lub BLIK-iem. Szybko, wygodnie i bezpiecznie.',
+  icons: {
+    icon: [
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
+      {
+        url: '/favicon.ico',
+        sizes: '32x32',
+        type: 'image/x-icon',
+      },
+    ],
+    apple: {
+      url: '/apple-icon.png',
+      type: 'image/png',
+      sizes: '180x180',
+    },
+  },
+};
 
 export default function RootLayout({
   children,
